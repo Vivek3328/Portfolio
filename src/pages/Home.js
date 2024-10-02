a92c380e088105a2b462853b504c196d0e539d8a
Vivek Mittal <mittal0623@gmail.com>
2024-10-02 22:13:30 +0530

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -48,38 +48,6 @@ const Home = () => {
             </h1>
             <div className="flex-none lg:flex my-[3rem] justify-around items-center	">
               <div className="w-full lg:w-1/2 p-5 home-about-description mx-auto">
-                {/* <p className="home-about-body m-[1rem] lg:mx-[3rem] ">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Python. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b>
-              </i>
-              and also in areas related to{" "}
-              <b className="purple">
-                Deep Learning and Natural Launguage Processing.
-              </b>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing
-              products with <b className="purple">Node.js</b> and{" "}
-              <i>
-                <b className="purple">
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
-            </p> */}
                 <div className="">
                   <div className="">
                     <blockquote className="blockquote sm:px-5  mb-0">
